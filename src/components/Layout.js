f3bfb0333f3e65a55b4bf073d4bdae79b67185ce
mmv <mmvsha73@gmail.com>
2019-02-28 18:00:12 +0400

    add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -61,11 +61,8 @@ const TemplateWrapper = props => (
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
           <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-
-          <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
           <meta name="theme-color" content="#fff" />
 
           <meta property="og:type" content="business.business" />
