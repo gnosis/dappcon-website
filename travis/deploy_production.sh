b8d9c8f1ef025dfc359c03bf2637d306ccaed2c7
Raul <raul@gnosis.pm>
2019-04-01 09:41:34 +0200

    Do not print curl output

--- travis/deploy_production.sh
+++ travis/deploy_production.sh
@@ -7,7 +7,8 @@ set -ev
 # - Security env variables are available.
 if [ -n "$LATEST_TAG" ] && [ -n "$PROD_DEPLOYMENT_HOOK_TOKEN" ] && [ -n "$PROD_DEPLOYMENT_HOOK_URL_DOMAIN" ]
 then
-  curl -X POST \
+  # Print only the request response code
+  curl --silent --output /dev/null --write-out "%{http_code}" -X POST \
      -F token="$PROD_DEPLOYMENT_HOOK_TOKEN" \
      -F ref=master \
      -F "variables[TRIGGER_RELEASE_COMMIT_TAG]=$LATEST_TAG" \
