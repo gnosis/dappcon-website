720e0580df856afe366f0778a68aff22fdfcc50c
Mikhail Mikheev <mdoublemmm@gmail.com>
2019-04-16 19:57:05 +0400

    update preview for index

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -2,19 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { IndexPageTemplate } from 'templates/index-page'
 
-const IndexPagePreview = ({ entry, fieldsMetaData }) => {
-  const speakers = { edges: [] }
+const getPropWithGraphQLStructure = propName => {
+  const values = { edges: [] }
+  const unformatted = fieldsMetaData.getIn([propName]).toJS()
 
-  if (fieldsMetaData.getIn(['speakers'])) {
-    const speakersUnformatted = fieldsMetaData.getIn(['speakers']).toJS()
-    speakers.edges = Object.values(speakersUnformatted).map(({ speakers }) => ({
+  if (unformatted) {
+    values.edges = Object.values(unformatted).map(edges => ({
       node: {
         frontmatter: {
-          ...Object.values(speakers)[0],
+          ...Object.values(edges[propName])[0],
         },
       },
     }))
   }
+
+  return values
+}
+
+const IndexPagePreview = ({ entry, fieldsMetaData }) => {
+  const speakers = getPropWithGraphQLStructure('speakers')
+  const sponsors = getPropWithGraphQLStructure('sponsors')
 
   return (
     <IndexPageTemplate
@@ -25,6 +32,7 @@ const IndexPagePreview = ({ entry, fieldsMetaData }) => {
       stats={entry.getIn(['data', 'stats']).toJS()}
       programPhotoText={entry.getIn(['data', 'programPhotoText'])}
       speakers={speakers}
+      sponsors={sponsors}
     />
   )
 }
