c15cd213413d5991cd60cff8a851f185b77675d8
Mikhail Mikheev <mdoublemmm@gmail.com>
2019-04-25 15:43:41 +0400

    dep bump and sort sponsors by package

--- src/components/IndexPage/SponsorsSection.js
+++ src/components/IndexPage/SponsorsSection.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from 'components/Button'
 import ContentWrapper from 'components/ContentWrapper'
 import { colors } from 'theme'
 
 const Wrapper = styled.section`
-  padding-bottom: 194px;
+  padding-bottom: 95px;
   background: ${colors.bgWhite};
 
   @media screen and (max-width: 767px) {
@@ -12,7 +13,18 @@ const Wrapper = styled.section`
   }
 `
 
-const SponsorContainer = styled.ul`
+const SponsorByTypeContainer = styled.div`
+  h3 {
+    font-size: 19px;
+    font-weight: 800;
+    margin-bottom: 12px;
+  }
+
+  text-align: center;
+  margin-bottom: 56px;
+`
+
+const SponsorList = styled.ul`
   display: grid;
   justify-content: center;
   grid-template-columns: ${props =>
@@ -21,7 +33,8 @@ const SponsorContainer = styled.ul`
 
   @media screen and (max-width: 767px) {
     grid-gap: 20px;
-    grid-template-columns: repeat(2, 130px);
+    grid-template-columns: ${props =>
+      `repeat(${props.sponsorsAmount < 2 ? props.sponsorsAmount : 2}, 130px)`};
   }
 `
 
@@ -35,13 +48,36 @@ const Img = styled.img`
     height: 60px;
   }
 `
+const ButtonLink = styled.a`
+  display: block;
+  width: 232px;
+  margin: 0 auto;
+
+  @media screen and (max-width: 767px) {
+    width: 190px;
+  }
+`
+
+const StyledButton = styled(Button)`
+  margin: 0 auto;
+  border: 3px solid ${colors.reddishPink};
+  color: ${colors.reddishPink};
+  font-size: 19px;
+  padding: 6px 13px;
+
+  @media screen and (max-width: 767px) {
+    font-size: 12px;
+    padding: 13px 26px;
+  }
+`
 
 const Sponsor = styled.li``
 
-const SponsorsSection = ({ sponsors }) => (
-  <Wrapper id="conferenceSponsors">
-    <ContentWrapper>
-      <SponsorContainer sponsorsAmount={sponsors.length}>
+const SponsorsListByType = ({ type, sponsors }) =>
+  sponsors.length ? (
+    <SponsorByTypeContainer>
+      <h3>{type} Sponsors</h3>
+      <SponsorList sponsorsAmount={sponsors.length}>
         {sponsors.map(sponsor => (
           <Sponsor key={sponsor.name}>
             <a href={sponsor.url} target="_blank" rel="noopener noreferrer">
@@ -49,9 +85,47 @@ const SponsorsSection = ({ sponsors }) => (
             </a>
           </Sponsor>
         ))}
-      </SponsorContainer>
-    </ContentWrapper>
-  </Wrapper>
-)
+      </SponsorList>
+    </SponsorByTypeContainer>
+  ) : null
+
+const SponsorsSection = ({ sponsors }) => {
+  const goldSponsors = []
+  const silverSponsors = []
+  const bronzeSponsors = []
+  const ironSponsors = []
+
+  for (let sponsor of sponsors) {
+    if (sponsor.type === 4) {
+      goldSponsors.push(sponsor)
+    }
+
+    if (sponsor.type === 3) {
+      silverSponsors.push(sponsor)
+    }
+
+    if (sponsor.type === 2) {
+      bronzeSponsors.push(sponsor)
+    }
+
+    if (sponsor.type === 1) {
+      ironSponsors.push(sponsor)
+    }
+  }
+
+  return (
+    <Wrapper id="conferenceSponsors">
+      <ContentWrapper>
+        <SponsorsListByType type="Gold" sponsors={goldSponsors} />
+        <SponsorsListByType type="Silver" sponsors={silverSponsors} />
+        <SponsorsListByType type="Bronze" sponsors={bronzeSponsors} />
+        <SponsorsListByType type="Iron" sponsors={ironSponsors} />
+        <ButtonLink href="mailto:sponsor@dappcon.io" rel="noopener noreferrer" style={{ zIndex: 2 }}>
+          <StyledButton text="BECOME A SPONSOR" hover={colors.secondaryBlack} />
+        </ButtonLink>
+      </ContentWrapper>
+    </Wrapper>
+  )
+}
 
 export default SponsorsSection
