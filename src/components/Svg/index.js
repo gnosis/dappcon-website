f0ff231db1913151f337d34d8794bdb7a3fa8c3e
Mikhail Mikheev <mmvsha73@gmail.com>
2020-02-11 20:16:02 +0400

    fixes

--- src/components/Svg/index.js
+++ src/components/Svg/index.js
@@ -1,6 +1,4 @@
 export { default as DappconLogoSmall } from './DappconLogoSmall'
 export { default as MobileMenuIcon } from './MobileMenu'
-export { default as EthInvader } from './EthInvader'
-export { default as DappconInvader } from './DappconInvader'
-export { default as EthLinesInvader } from './EthLinesInvader'
 export { default as DownloadIcon } from './Download'
+export { default as DappconIllus } from './DappconIllus'
