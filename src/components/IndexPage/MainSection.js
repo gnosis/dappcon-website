ae032ec0a9222ae5e35aafa3ecede0b7d5ccfc27
Mikhail Mikheev <mmvsha73@gmail.com>
2020-03-23 18:54:44 +0400

    hoverable illustration

--- src/components/IndexPage/MainSection.js
+++ src/components/IndexPage/MainSection.js
@@ -4,7 +4,8 @@ import Markdown from "react-markdown"
 import { colors } from "theme"
 import ButtonLink from "components/ButtonLink"
 import ContentWrapper from "components/ContentWrapper"
-import { DappconIllus } from 'components/Svg'
+import { DappconIllus } from "components/Svg"
+import { Link } from "gatsby"
 
 const Wrapper = styled.section`
   height: 100vh;
@@ -22,6 +23,68 @@ const Img = styled.img`
   width: 100%;
 `
 
+const SDappconIllus = styled(DappconIllus)`
+  .st0 {
+    fill: #141314;
+  }
+  .st1 {
+    font-family: "Averta";
+    font-weight: 800;
+  }
+  .st2 {
+    font-size: 72px;
+  }
+  .st3 {
+    font-size: 16px;
+  }
+  .st4 {
+    fill-rule: evenodd;
+    clip-rule: evenodd;
+  }
+  .st5 {
+    filter: url(#Adobe_OpacityMaskFilter);
+  }
+  .st6 {
+    fill-rule: evenodd;
+    clip-rule: evenodd;
+    fill: #ffffff;
+  }
+  .st7 {
+    mask: url(#b_1_);
+    fill-rule: evenodd;
+    clip-rule: evenodd;
+  }
+  .st8 {
+    filter: url(#Adobe_OpacityMaskFilter_1_);
+  }
+  .st9 {
+    mask: url(#d_1_);
+    fill-rule: evenodd;
+    clip-rule: evenodd;
+  }
+  .st10 {
+    font-size: 10px;
+  }
+  .tickets-text {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out, fill 0.3s ease-in-out;
+  }
+
+  .hoverable {
+    transition: fill 0.3s ease-in-out;
+  }
+  &:hover {
+    .tickets-text {
+      opacity: 1;
+      fill: red;
+    }
+
+    .hoverable {
+      fill: red;
+    }
+  }
+`
+
 const MainSection = ({
   mainTitle,
   buttonText,
@@ -30,7 +93,9 @@ const MainSection = ({
 }) => (
   <Wrapper id="main">
     <SContentWrapper>
-      <DappconIllus />
+      <Link to="/tickets">
+        <SDappconIllus />
+      </Link>
     </SContentWrapper>
   </Wrapper>
 )
